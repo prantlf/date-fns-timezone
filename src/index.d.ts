49a1ab07c30331636c497f004d73bfd4240adee9
Leonardo Gatica <lgatica@protonmail.com>
2019-01-30 15:35:28 -0300

    feat(typings): add FormatTimeZoneOptions extended of FormatOptions
    Set FormatTimeZoneOptions in formatToTimeZone

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,6 +2,12 @@ type DateInput = string | number | Date
 interface TimeZoneOptions {
   timeZone: string
 }
+interface FormatOptions {
+  locale?: Object
+}
+interface FormatTimeZoneOptions extends FormatOptions {
+  timeZone: string
+}
 
 declare function convertToLocalTime (dateInput: DateInput, options: TimeZoneOptions): Date
 declare function convertToTimeZone (dateInput: DateInput, options: TimeZoneOptions): Date
@@ -10,7 +16,7 @@ declare function parseFromString (dateString: string, format: string): Date
 declare function parseFromTimeZone (dateString: string, format: string, options: TimeZoneOptions): Date
 declare function parseFromTimeZone (dateString: string, options: TimeZoneOptions): Date
 
-declare function formatToTimeZone (dateInput: DateInput, format: string, options: TimeZoneOptions): string
+declare function formatToTimeZone (dateInput: DateInput, format: string, options: FormatTimeZoneOptions): string
 
 export {
   convertToLocalTime, convertToTimeZone, parseFromString, parseFromTimeZone, formatToTimeZone
